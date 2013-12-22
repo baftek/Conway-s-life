18a0d20404b84ae744ef0387e60fb372fe63e8cf
Baftek <baftekads@gmail.com>
2013-12-22 23:57:22 +0100

    keyboard events, text in allegro window, close button, new fancy error boxes

--- Conway's life/main.cpp
+++ Conway's life/main.cpp
@@ -6,18 +6,19 @@
 	 Tutor: Piotr Fabian
 	 Copyright (c) 2013, Bartlomiej Wolny. All rights reserved. 
 */
-#define ALLEGRO_STATICLINK
+//#define ALLEGRO_STATICLINK
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <allegro5\allegro5.h>
-#include <allegro5\allegro_image.h>
+//#include <allegro5\allegro_image.h>
 #include <allegro5\allegro_native_dialog.h>
 #include <allegro5\allegro_primitives.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
 ALLEGRO_DISPLAY *display = NULL;
-ALLEGRO_BITMAP  *image   = NULL;
-//#include <allegro5\allegro.h>
-//#include <allegro5\allegro_image.h>
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+//ALLEGRO_BITMAP  *image   = NULL;
 
 #define CURRENT_ARRAY (!(generation_number%2))
 
@@ -229,33 +230,57 @@ int read_starting_positions_from_file()
 
 int main()
 {	
-	printf("\n\n	Conway's life by baftek\n\n	This is a program that simulates a living group of cells.\n	Dots are dead cells, hashes are alive cells.\n	Alive cells die from isolation when they have 0-1 neighbours\n	They also die of overcrowd when they have 4 or more neighbours.\n	They becomes alive when they have exactly 3 neighbours.\n	Press ANY KEY to start. New window will appear.");
+	//printf("\n\n	Conway's life by baftek\n\n	This is a program that simulates a living group of cells.\n	Dots are dead cells, hashes are alive cells.\n	Alive cells die from isolation when they have 0-1 neighbours\n	They also die of overcrowd when they have 4 or more neighbours.\n	They becomes alive when they have exactly 3 neighbours.\n	Press ANY KEY to start. New window will appear.");
 	if(!read_starting_positions_from_file())
 		if(_getch())
 			return 0;
-	
-	_getch();
-
  
 	if(!al_init()) 
 	{
-		fprintf(stderr, "failed to initialize allegro!\n");
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", 
+                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return -1;
 	}
 
-	if(!al_init_image_addon()) 
+	/*if(!al_init_image_addon()) 
 	{
       al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", 
                                  NULL, ALLEGRO_MESSAGEBOX_ERROR);
       return 0;
-	}
- 
-	display = al_create_display(640, 480);
+	}*/
+
+	display = al_create_display(730, 480);
 	if(!display) 
 	{
-		fprintf(stderr, "failed to create display!\n");
+		al_show_native_message_box(display, "Error", "Error", "failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		return -1;
 	}
+
+	event_queue = al_create_event_queue();
+	if(!event_queue) 
+	{
+		al_show_native_message_box(display, "Error", "Error", "failed to create event_queue!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return -1;
+	}
+
+	al_init_font_addon();
+	al_init_ttf_addon();
+	#define TEXT_SIZE 17
+	ALLEGRO_FONT *font = al_load_ttf_font("C:\\Windows\\Fonts\\cour.ttf", TEXT_SIZE, 0);
+	if(!font)
+	{
+		al_destroy_event_queue(event_queue);
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize font!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		_getch();
+		return -1;
+	}
+
+	al_install_keyboard();
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	ALLEGRO_EVENT ev;
 
 	/*image = al_load_bitmap(".\\dead.jpg"); //C:\\Users\\Bartek\\Documents\\Visual Studio 2010\\Projects\\Conway's life\\Debug
 	if(!image) {
@@ -267,13 +292,32 @@ int main()
  
 	al_init_primitives_addon();
 	al_clear_to_color(al_map_rgb(0,0,20));
-	
-	do
+	al_flip_display();
+
+#define CALC_POS_Y(x) ( 50+((x)*(TEXT_SIZE+2)) )
+#define XPOS 25
+#define TEXT_COLOUR 255, 255, 255
+
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(0), 0, "Conway's life");
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(1), 0, "This is a program that simulates a living group of cells.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(2), 0, "Alive cells (yellow circles) die from isolation when they have 0-1 neighbours");
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(3), 0, "They also die of overcrowd when they have 4 or more neighbours.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(4), 0, "They becomes alive when they have exactly 3 neighbours.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(5), 0, "Press ANY KEY to start.");
+	al_flip_display();
+
+	while(1)
 	{
+		al_flush_event_queue(event_queue);
+		al_wait_for_event(event_queue, &ev);
+		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE || (ev.type == ALLEGRO_EVENT_KEY_DOWN && (ev.keyboard.keycode == ALLEGRO_KEY_Q || ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)))
+			goto program_exit;
+		else
+			al_flush_event_queue(event_queue); 
+
 		draw_in_console(generation_number);
 		recalculate_environment();
 		generation_number++;
-		
 
 		if(alive_cells_number == 0)
 		{
@@ -286,9 +330,12 @@ int main()
 			alive_cells_number = 0;
 			printf("\nPress any key to continue to next generation\nor press q to quit");
 		}
+	}
 
-	} while(_getch()!='q');
-
-	//al_destroy_display(display);
+program_exit:
+	al_destroy_event_queue(event_queue);
+	al_destroy_display(display);
 	return 0;
 }
+
+//TODO: dynamic window size, dynamic grid size, avoid console window, move all text into allegro window
