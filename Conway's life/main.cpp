54748d0b790454bd162322b078baf58abe65d30a
Baftek <baftekads@gmail.com>
2013-12-23 23:06:50 +0100

    borders meet, filename argument, more text in allegro window, keypress stability, code tidy up

--- Conway's life/main.cpp
+++ Conway's life/main.cpp
@@ -6,25 +6,31 @@
 	 Tutor: Piotr Fabian
 	 Copyright (c) 2013, Bartlomiej Wolny. All rights reserved. 
 */
-//#define ALLEGRO_STATICLINK
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <allegro5\allegro5.h>
-//#include <allegro5\allegro_image.h>
 #include <allegro5\allegro_native_dialog.h>
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-//ALLEGRO_BITMAP  *image   = NULL;
+ALLEGRO_FONT *font = NULL;
+
+#define CALC_POS_Y(x) ( 50+((x)*(TEXT_SIZE+2)) )
+#define XPOS 25 // x axis position of welcome text
+#define TEXT_COLOR 255, 255, 255
 
 #define CURRENT_ARRAY (!(generation_number%2))
 
 int total_rows=0;
 int total_columns=0;
-char ***cell_map;
+char ***cell_map; 
+char should_borders_be_considered = 1;
+char file_not_found = 0;
+char filename[] = "input.txt";
+char *pointer_to_filename = filename;
 
 int alive_cells_number = 0;
 int generation_number = 1;
@@ -37,24 +43,20 @@ int check_ndb(int row, int column)
 	// 678
 	int nbd_counter=0;
 
-	if(row > 0 && column > 0) 
-		if(cell_map[CURRENT_ARRAY][row-1][column-1]==1 ) nbd_counter++;
-	if(row > 0)
-		if(cell_map[CURRENT_ARRAY][row-1][column  ]==1 ) nbd_counter++;
-	if(row > 0 && column < total_columns-1)
-		if(cell_map[CURRENT_ARRAY][row-1][column+1]==1 ) nbd_counter++;
-	if(column > 0)
-		if(cell_map[CURRENT_ARRAY][row  ][column-1]==1 ) nbd_counter++;
-	if(column < total_columns-1)
-		if(cell_map[CURRENT_ARRAY][row  ][column+1]==1 ) nbd_counter++;
-	if(row < total_rows-1 && column > 0)
-		if(cell_map[CURRENT_ARRAY][row+1][column-1]==1 ) nbd_counter++;
-	if(row < total_rows-1)
-		if(cell_map[CURRENT_ARRAY][row+1][column  ]==1 ) nbd_counter++;
-	if(row < total_rows-1 && column < total_columns-1)
-		if(cell_map[CURRENT_ARRAY][row+1][column+1]==1 ) nbd_counter++;
-	//if(row > total_rows || column > total_columns)
-		//printf("row lub column poza zakresem!!!", _getch());
+	// if checking should not exceed borders it's enough to leave only the false fragment in defines
+#define ROW_CHECK_MINUS (row==0 ? total_rows-1 : row-1) // if row==0 then we must also check last row
+#define COL_CHECK_MINUS (column==0 ? total_columns-1 : column-1)
+#define ROW_CHECK_PLUS (row+1==total_rows ? 0 : row+1) // if row==total_columns then we must also check 0'th row
+#define COL_CHECK_PLUS (column+1==total_columns ? 0 : column+1)
+
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_MINUS][COL_CHECK_MINUS]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_MINUS][column  ]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_MINUS][COL_CHECK_PLUS]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][row  ][COL_CHECK_MINUS]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][row  ][COL_CHECK_PLUS]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_PLUS][COL_CHECK_MINUS]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_PLUS][column  ]==1 ) nbd_counter++;
+	if(cell_map[CURRENT_ARRAY][ROW_CHECK_PLUS][COL_CHECK_PLUS]==1 ) nbd_counter++;
 
 	return nbd_counter;
 }
@@ -62,7 +64,7 @@ int check_ndb(int row, int column)
 ///////////////////////////////////
 #define GRID_SIZE 15
 #define POSITION_OFFSET 50
-int draw_in_console(int generation_number)
+int draw_result(int generation_number)
 {
 	system("cls");
 	printf("Conway's life			  GENERATION: %d\n\n	", generation_number);
@@ -73,20 +75,19 @@ int draw_in_console(int generation_number)
 		al_draw_line(POSITION_OFFSET+i*GRID_SIZE, POSITION_OFFSET, POSITION_OFFSET+i*GRID_SIZE, total_rows*GRID_SIZE+POSITION_OFFSET, al_map_rgb(25, 255, 100), 1);
 	for(int i=1; i<total_rows; i++)
 		al_draw_line(POSITION_OFFSET, POSITION_OFFSET+i*GRID_SIZE, total_columns*GRID_SIZE+POSITION_OFFSET, i*GRID_SIZE+POSITION_OFFSET, al_map_rgb(25, 255, 100), 1);
-	
+	al_draw_textf(font, al_map_rgb(TEXT_COLOR), 50, 25, 0, "Conway's life by baftek        Generation %d", generation_number);
 
 	for(int i=0; i < total_rows; i++)
 	{
 		for(int j=0; j < total_columns; j++)
 		{
 			if(cell_map[CURRENT_ARRAY][i][j])
+				//al_draw_textf(font, al_map_rgb(255, 255, 0), 50+j*GRID_SIZE, 50+i*GRID_SIZE-3, 0, "%d", check_ndb(i, j));
 				al_draw_filled_circle(50+(GRID_SIZE/2)+j*GRID_SIZE, 50+(GRID_SIZE/2)+i*GRID_SIZE, 5, al_map_rgb(255, 255, 0));
 			else
 				al_draw_filled_circle(50+(GRID_SIZE/2)+j*GRID_SIZE, 50+(GRID_SIZE/2)+i*GRID_SIZE, 5, al_map_rgb(80, 50, 50));
 		}
 	}
-
-	al_flip_display();
 
 	//printf("\n\n	");
 	for(int i=0; i<total_rows; i++)
@@ -98,6 +99,17 @@ int draw_in_console(int generation_number)
 		}
 		printf("\n	");
 	}
+
+	if(alive_cells_number == 0 && generation_number != 1)
+		al_draw_text(font, al_map_rgb(255, 220, 220), 50, total_rows*GRID_SIZE+POSITION_OFFSET+50, 0, "All cells are dead. Press Q or escape to quit.");
+	else
+	{
+		alive_cells_number = 0;
+		printf("\nPress any key to continue to next generation\nor press q to quit");
+		al_draw_text(font, al_map_rgb(TEXT_COLOR), 50, (total_rows*GRID_SIZE+POSITION_OFFSET+50), 0, "Press any key to continue to next generation or Q to quit.");
+	}
+
+	al_flip_display();
 	return 1;
 }
 
@@ -140,20 +152,16 @@ int recalculate_environment()
 }
 
 ////////////////////
-#define FILENAME ".\\input.txt"
-////////////////////
-int read_starting_positions_from_file()
+int read_starting_positions_from_file(int argc)
 {
 	FILE *f;
-	char file_found;
-	if(file_found = !(f = fopen(FILENAME, "r")))
+	if(file_not_found = !(f = fopen(pointer_to_filename, "r")))
 	{
-		printf("\n\n\n   Could not open the file %s\n\n   Random values will be generated.\n", FILENAME);
-		srand(time(NULL)); // init of rand
-		total_columns = rand() % 30 + 10;
-		total_rows = rand() % 25 + 10;
+		printf("\n\n\n   Could not open the file %s\n\n   Random values will be generated.\n", pointer_to_filename);
+		//srand(time(NULL));	// init of rand
+		total_columns = 45; //rand() % 15 + 30;
+		total_rows = 30;	// rand() % 25 + 10;
 		printf("\n   Generated numbers\n   columns = %d\n   rows = %d", total_columns, total_rows);
-		_getch();
 		//return 0;
 	}
 	else //file exists
@@ -163,9 +171,7 @@ int read_starting_positions_from_file()
 		// count columns
 		while(fgetc(f) != '\n')
 			total_columns++;
-		//total_columns+=1; // korekta
-		fclose(f);
-		f=fopen(FILENAME, "r");
+		fseek(f, 0, SEEK_SET);
 
 		//count rows
 		while(!feof(f))
@@ -202,10 +208,10 @@ int read_starting_positions_from_file()
 			}
 		}
 
-		if(!file_found) // 0-not, !0-yes
+		if(!file_not_found) // if found
 		{
 			// reading file and saving data to array
-			f = fopen(FILENAME, "r"); 
+			f = fopen(pointer_to_filename, "r"); 
 			for(int current_row=0; current_row < total_rows; current_row++) // in every row
 			{	
 				for(int current_column=0; current_column < total_columns; current_column++) // for every column
@@ -215,7 +221,7 @@ int read_starting_positions_from_file()
 			}
 			fclose(f);
 		}
-		else
+		else //random cell values generating
 		{
 			for(int current_row=0; current_row < total_rows; current_row++) // in every row
 			{	
@@ -224,16 +230,15 @@ int read_starting_positions_from_file()
 			}
 		}
 
-	//_getch();
 	return 1;
 }
 
-int main()
+int main(int argc, char *argv[])
 {	
+	if(argc > 1)
+		pointer_to_filename = argv[1];
 	//printf("\n\n	Conway's life by baftek\n\n	This is a program that simulates a living group of cells.\n	Dots are dead cells, hashes are alive cells.\n	Alive cells die from isolation when they have 0-1 neighbours\n	They also die of overcrowd when they have 4 or more neighbours.\n	They becomes alive when they have exactly 3 neighbours.\n	Press ANY KEY to start. New window will appear.");
-	if(!read_starting_positions_from_file())
-		if(_getch())
-			return 0;
+	read_starting_positions_from_file(argc);
  
 	if(!al_init()) 
 	{
@@ -242,14 +247,7 @@ int main()
 		return -1;
 	}
 
-	/*if(!al_init_image_addon()) 
-	{
-      al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", 
-                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
-      return 0;
-	}*/
-
-	display = al_create_display(730, 480);
+	display = al_create_display(800, 600);
 	if(!display) 
 	{
 		al_show_native_message_box(display, "Error", "Error", "failed to create display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
@@ -263,17 +261,17 @@ int main()
 		al_destroy_display(display);
 		return -1;
 	}
+	al_flush_event_queue(event_queue);
 
 	al_init_font_addon();
 	al_init_ttf_addon();
-	#define TEXT_SIZE 17
-	ALLEGRO_FONT *font = al_load_ttf_font("C:\\Windows\\Fonts\\cour.ttf", TEXT_SIZE, 0);
+#define TEXT_SIZE 17
+	font = al_load_ttf_font("C:\\Windows\\Fonts\\cour.ttf", TEXT_SIZE, 0);
 	if(!font)
 	{
 		al_destroy_event_queue(event_queue);
 		al_show_native_message_box(display, "Error", "Error", "Failed to initialize font!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		al_destroy_display(display);
-		_getch();
 		return -1;
 	}
 
@@ -281,29 +279,23 @@ int main()
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 	ALLEGRO_EVENT ev;
-
-	/*image = al_load_bitmap(".\\dead.jpg"); //C:\\Users\\Bartek\\Documents\\Visual Studio 2010\\Projects\\Conway's life\\Debug
-	if(!image) {
-      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", 
-                                 NULL, ALLEGRO_MESSAGEBOX_ERROR);
-      al_destroy_display(display);
-      return 0;
-   }*/
  
 	al_init_primitives_addon();
 	al_clear_to_color(al_map_rgb(0,0,20));
 	al_flip_display();
 
-#define CALC_POS_Y(x) ( 50+((x)*(TEXT_SIZE+2)) )
-#define XPOS 25
-#define TEXT_COLOUR 255, 255, 255
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(0), 0, "Conway's life");
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(1), 0, "This is a program that simulates a living group of cells.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(2), 0, "Alive cells (yellow circles) die from isolation when they have 0-1 neighbours");
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(3), 0, "They also die of overcrowd when they have 4 or more neighbours.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(4), 0, "They becomes alive when they have exactly 3 neighbours.");
+	al_draw_text(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(6), 0, "Press ANY KEY to start.");
+	if(file_not_found)
+		al_draw_textf(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(10), 0, "No desired file found, generated random data:");
+	else
+		al_draw_textf(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(10), 0, "File found: ");
+		al_draw_textf(font, al_map_rgb(TEXT_COLOR), XPOS, CALC_POS_Y(11), 0, "%d rows, %d columns", total_rows, total_columns);
 
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(0), 0, "Conway's life");
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(1), 0, "This is a program that simulates a living group of cells.");
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(2), 0, "Alive cells (yellow circles) die from isolation when they have 0-1 neighbours");
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(3), 0, "They also die of overcrowd when they have 4 or more neighbours.");
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(4), 0, "They becomes alive when they have exactly 3 neighbours.");
-	al_draw_text(font, al_map_rgb(TEXT_COLOUR), XPOS, CALC_POS_Y(5), 0, "Press ANY KEY to start.");
 	al_flip_display();
 
 	while(1)
@@ -311,31 +303,17 @@ int main()
 		al_flush_event_queue(event_queue);
 		al_wait_for_event(event_queue, &ev);
 		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE || (ev.type == ALLEGRO_EVENT_KEY_DOWN && (ev.keyboard.keycode == ALLEGRO_KEY_Q || ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE)))
-			goto program_exit;
-		else
-			al_flush_event_queue(event_queue); 
+			break;  //exit program
 
-		draw_in_console(generation_number);
+		while(ev.type != ALLEGRO_EVENT_KEY_UP)
+			al_wait_for_event(event_queue, &ev);
+			
+		draw_result(generation_number);
 		recalculate_environment();
 		generation_number++;
-
-		if(alive_cells_number == 0)
-		{
-			printf("\n  All cells are dead.\n  Press q to quit.");
-			if(_getch() == 'q')
-				break;
-		}
-		else
-		{
-			alive_cells_number = 0;
-			printf("\nPress any key to continue to next generation\nor press q to quit");
-		}
 	}
 
-program_exit:
 	al_destroy_event_queue(event_queue);
 	al_destroy_display(display);
 	return 0;
 }
-
-//TODO: dynamic window size, dynamic grid size, avoid console window, move all text into allegro window
